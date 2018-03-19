ffcc66be04a95c81aad674a7237a0a9e6b6e17d1
it-brown <jonah4mjst@gmail.com>
2018-03-19 16:50:38 +0900

    initial commit

---
+++ src/components/entry/Index.vue
@@ -0,0 +1,35 @@
+<template lang='pug'>
+.vue-index
+    .reactive-title {{ reactiveTitle() }}
+    sample
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator';
+import VueUtil from '@/scripts/util/VueUtil';
+import RootVue from '@/components/base/RootVue';
+import Sample from '@/components/sample/Sample.vue';
+
+import Buefy from 'buefy';
+
+Vue.use(Buefy);
+
+/**
+ * Vue Component
+ */
+@Component
+export default class Index extends RootVue {
+    public title: string = 'index';
+
+    protected beforeCreate(): void {
+        // Inner Vue 登録
+        VueUtil.registerComponents([Sample]);
+    }
+}
+</script>
+
+<style lang='sass'>
+@import 'all'
+
+.vue-index
+</style>
