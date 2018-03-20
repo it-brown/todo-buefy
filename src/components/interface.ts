c317630946bd058e8de5c6e073af0112e56fab68
it-brown <jonah4mjst@gmail.com>
2018-03-20 13:58:11 +0900

    define interface

---
+++ src/components/interface.ts
@@ -0,0 +1,12 @@
+export interface InputFormItemProps {
+    title: string,
+    type: string
+}
+
+export interface taskItemProps {
+    id: number,
+    time_stamp: Date,
+    name: string,
+    doing: boolean,
+    editing: boolean
+}
